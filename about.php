b3f974edfeb3b36ce1d0b0fed13ba181b9c79ffe
CharlieLucy <88269667+CharlieLucy@users.noreply.github.com>
2022-04-08 14:12:49 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,7 +1,14 @@
 <?php include('./header.php'); ?>
 
 <div class="container">
-    Type here
+    <h1>Our Story</h1>
+    <br>
+    <p>We are are a group of people from Hong Kong and we all like reading books.
+    <p>However, books in physical forms require a lot of space to store which is diifficult to do in Hong Kong.
+    <p>Therefore, we created this website to sell e-books to people in Hong Kong.
+    <p>We believe that e-books are easier to store and carry around in a phone so the convenience e-books brings to the table would encourage more people to read books.
+    <br>
+
 </div>
 
 <?php include('./footer.php'); ?>
