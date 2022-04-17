3d1f7735a9d88204e8f622e27fd9f7171d4f20fa
jackwck <39961125+jackwck@users.noreply.github.com>
2022-04-18 00:02:35 +0800

    Add files via upload

--- about.php
+++ about.php
@@ -1,13 +1,71 @@
 <?php include('./header.php'); ?>
 
 <div class="container">
-    <h1>Our Story</h1>
-    <br>
-    <p>We are are a group of people from Hong Kong and we all like reading books.
-    <p>However, books in physical forms require a lot of space to store which is diifficult to do in Hong Kong.
-    <p>Therefore, we created this website to sell e-books to people in Hong Kong.
-    <p>We believe that e-books are easier to store and carry around in a phone so the convenience e-books brings to the table would encourage more people to read books.
-    <br>
+
+	<br>
+	<br>
+	
+	<div class="row story">
+		<div class="card">
+		<h1 class="card-title">Our Story</h1>
+		<br>
+		<p>We are are a group of people from Hong Kong and we all like reading books.
+		<p>However, books in physical forms require a lot of space to store which is diifficult to do in Hong Kong.
+		<p>Therefore, we created this website to sell e-books to people in Hong Kong.
+		<p>We believe that e-books are easier to store and carry around in a phone so the convenience e-books brings to the table would encourage more people to read books.
+		<br>
+		</div>
+	</div>
+	
+	<br>
+	<br>
+	<br>
+	
+	<div class="row people">
+		<div class="col-sm">
+			<div class="card">
+				<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="Card image">
+				<div class="card-body">
+					<h4 class="card-title">AAA</h4>
+					<h4 class="card-title">CEO of e-book shop</h4>
+					<p class="card-text">Undergraduate of HK PolyU.</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm">
+			<div class="card">
+				<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar2.png" alt="Card image">
+				<div class="card-body">
+					<h4 class="card-title">BBB</h4>
+					<h4 class="card-title">CFO of e-book shop</h4>
+					<p class="card-text">Undergraduate of HK PolyU.</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm">
+			<div class="card">
+				<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar3.png" alt="Card image">
+				<div class="card-body">
+					<h4 class="card-title">CCC</h4>
+					<h4 class="card-title">HR of e-book shop</h4>
+					<p class="card-text">Undergraduate of HK PolyU.</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm">
+			<div class="card">
+				<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar4.png" alt="Card image">
+				<div class="card-body">
+					<h4 class="card-title">DDD</h4>
+					<h4 class="card-title">I.T. of e-book shop</h4>
+					<p class="card-text">Undergraduate of HK PolyU.</p>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </div>
 
--- help.php
+++ help.php
@@ -1,7 +1,55 @@
 <?php include('./header.php'); ?>
 
 <div class="container">
-    Type here
+
+	<div class="row title">
+		<h1>FAQ</h1>
+		
+	</div>
+	
+	<div class="row">
+		<div id="q1">
+			<div class="card card-body bg-primary text-white">
+				How to purchase?
+			</div>
+		</div>
+		<div id="a1">
+			<div class="card card-body">
+				From the homepage, add goods to cart, and click checkout!
+			</div>
+		</div>
+	</div>
+	<br>
+	
+	<div class="row">
+		<div id="q2">
+			<div class="card card-body bg-primary text-white">
+				How to refund?
+			</div>
+		</div>
+		<div id="a2">
+			<div class="card card-body">
+				As it is a digital good, it cannot be refunded.
+			</div>
+		</div>
+	</div>
+	<br>
+	
+	<div class="row">
+		<div id="q3">
+			<div class="card card-body bg-primary text-white">
+				How to contact for support?
+			</div>
+		</div>
+		<div id="a3">
+			<div class="card card-body">
+				<div style="display:inline">
+					You can send an email to us via <a href="mailto:gp14@gmail.com">gp14@gmail.com</a>, or call us on +852 87654321.
+				</div>
+			</div>
+		</div>
+	</div>
+
 </div>
 
 <?php include('./footer.php'); ?>
